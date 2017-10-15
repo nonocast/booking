0a5164622d1a931dfb4777d6e7d7aab4a39ada06
nonocast <nonocast@gmail.com>
2017-10-16 01:45:39 +0800

    add send mail test code

--- web/booking/src/main/java/cn/nonocast/BookingApplication.java
+++ web/booking/src/main/java/cn/nonocast/BookingApplication.java
@@ -1,5 +1,10 @@
 package cn.nonocast;
 
+import org.simplejavamail.email.Email;
+import org.simplejavamail.email.EmailBuilder;
+import org.simplejavamail.mailer.Mailer;
+import org.simplejavamail.mailer.config.ServerConfig;
+import org.simplejavamail.mailer.config.TransportStrategy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -12,6 +17,24 @@ import org.springframework.web.bind.annotation.RestController;
 public class BookingApplication extends SpringBootServletInitializer {
 	@RequestMapping("/")
 	public String home() { return "hello world, booking"; }
+
+
+	@RequestMapping("/sendmail")
+	public String sendmail() {
+		Email email = new EmailBuilder()
+				.from("caohui", "caohui@shgbit.com")
+				.to("caohui", "caohui@shgbit.com")
+				.subject("hello world, from simple java mail")
+				.text("We should meet up! ;)")
+				.build();
+
+		new Mailer(
+				new ServerConfig("smtp.exmail.qq.com", 465, "caohui@shgbit.com", ""),
+				TransportStrategy.SMTP_SSL
+		).sendMail(email);
+		return "Sent.";
+	}
+
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {

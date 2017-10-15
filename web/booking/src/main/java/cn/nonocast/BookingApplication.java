a580221e562b30961fb8dbff69a3dc56c8e4c56b
nonocast <nonocast@gmail.com>
2017-10-16 00:39:15 +0800

    fix

--- web/booking/src/main/java/cn/nonocast/BookingApplication.java
+++ web/booking/src/main/java/cn/nonocast/BookingApplication.java
@@ -2,14 +2,21 @@ package cn.nonocast;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class BookingApplication {
+public class BookingApplication extends SpringBootServletInitializer {
 	@RequestMapping("/")
-	public String home() { return "hello world"; }
+	public String home() { return "hello world, booking"; }
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BookingApplication.class);
+	}
 
 
 	public static void main(String[] args) {
